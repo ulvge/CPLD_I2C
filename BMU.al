0e435df373052d8313d7107e4402c9d385e64dee
zhouBin <ulvge@126.com>
2023-12-07 19:15:38 +0800

    compile success

--- BMU.al
+++ BMU.al
@@ -1,12 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<Project Version="1" Path="E:/3Proj/17BMU/NS213_BMU">
+<Project Version="3" Minor="2" Path="E:/3Proj/17BMU/NS213_BMU">
     <Project_Created_Time></Project_Created_Time>
-    <TD_Version>5.0.30786</TD_Version>
+    <TD_Version>5.6.86856</TD_Version>
     <UCode>00000000</UCode>
     <Name>BMU</Name>
     <HardWare>
         <Family>EF2</Family>
-        <Device>EF2L45BG256B</Device>
+        <Device>EF2L45UG132B</Device>
+        <Speed></Speed>
     </HardWare>
     <Source_Files>
         <Verilog>
@@ -47,28 +48,42 @@
         </ADC_FILE>
     </Source_Files>
     <FileSets>
-        <FileSet Name="constrain_1" Type="ConstrainFiles">
-        </FileSet>
         <FileSet Name="design_1" Type="DesignFiles">
+        </FileSet>
+        <FileSet Name="constrain_1" Type="ConstrainFiles">
         </FileSet>
     </FileSets>
     <TOP_MODULE>
         <LABEL></LABEL>
-        <MODULE>NS213_BMU_cpld_top</MODULE>
-        <CREATEINDEX>auto</CREATEINDEX>
+        <MODULE>ns213_bmu_cpld_top</MODULE>
+        <CREATEINDEX>user</CREATEINDEX>
     </TOP_MODULE>
     <Property>
-        <RtlProperty>
-            <keep_hierarchy>manual</keep_hierarchy>
-        </RtlProperty>
     </Property>
     <Device_Settings>
     </Device_Settings>
     <Configurations>
     </Configurations>
+    <Runs>
+        <Run Name="syn_1" Type="Synthesis" ConstraintSet="constrain_1" Description="" Active="true">
+            <Strategy Name="Default_Synthesis_Strategy">
+                <RtlProperty>
+                    <keep_hierarchy>manual</keep_hierarchy>
+                </RtlProperty>
+            </Strategy>
+            <UserParams>
+            </UserParams>
+        </Run>
+        <Run Name="phy_1" Type="PhysicalDesign" ConstraintSet="constrain_1" Description="" SynRun="syn_1" Active="true">
+            <Strategy Name="Default_PhysicalDesign_Strategy">
+            </Strategy>
+            <UserParams>
+            </UserParams>
+        </Run>
+    </Runs>
     <Project_Settings>
-        <Step_Last_Change>2023-12-07 17:01:53.597</Step_Last_Change>
-        <Current_Step>60</Current_Step>
+        <Step_Last_Change>2023-12-07 17:41:50.586</Step_Last_Change>
+        <Current_Step>0</Current_Step>
         <Step_Status>true</Step_Status>
     </Project_Settings>
 </Project>
--- src/top.v
+++ src/top.v
@@ -1,55 +1,109 @@
 `timescale 1ns/1ps
 `include "src/io_deglitch.v"
 
-module NS213_BMU_cpld_top
+module ns213_bmu_cpld_top
 (
-    input CPLD_CLK_50M,
-    input CPLD_RST_N,
-
-    input SCL,
-    inout SDA,
-
-    output CPLD_DEBUG3,
-    output CPLD_DEBUG4,
+    input FPGA_CLK_50M,
+    //input CPLD_RST_N,
 
     input CPLD_LED0_N,
     output CPLD_LED1_N,
     output CPLD_LED2_N,
     input CPLD_LED12_N,
 
+    input BMC_GPIO0,
+    input BMC_GPIO1,
+    input BMC_GPIO2,
+    input BMC_GPIO3,
+
+    input GPIO_RSVD1,// unsed
+    input GPIO_RSVD2,// unsed
+    input QSPI_CSN0,
+    
+    output QSPI_CSN0_FPGA,
+    output QSPI_CSN1_FPGA,
+    
+    input R_BMC_I2C_SCL1,
+    inout R_BMC_I2C_SDA1,
+
+    input R_BMC_PCIE_RST_N, // BMC_PCIE复位
+    input R_BMC_PHY_RST_N, // PHY复位
+    input R_BMC_RSTN_EXT,   // BMC复位
+    output R_BMC_RSTN_FPGA,
+    input R_CPU_POR_N,
+
+    output USB_SWITCH_EN,
+
+    output CPLD_DEBUG3,
+    output CPLD_DEBUG4,
+
+    input R_RESERVE0,
+    input R_RESERVE1,
+    input R_RESERVE2,
+    input R_RESERVE3,
+    input R_RESERVE4,
+    input R_RESERVE5,
+    input R_RESERVE6,
+    input R_RESERVE7,
+    input R_RESERVE8,
+    input R_RESERVE9,
+    input R_RESERVE10,
+    input R_RESERVE11,
+    input R_RESERVE12,
+    input R_RESERVE13,
+    input R_RESERVE14,
+    input R_RESERVE15,
+    input R_RESERVE16,
+    input R_RESERVE17,
+    input R_RESERVE18,
+    input R_RESERVE19,
+
+    input VCORE_EN,
+    input P1V8_EN,
+    input P1V1_EN,
+    input P3V3_EN,
+    input VCORE_PWRGD,
+    input P1V8_PWRGD,
+    input P3V3_PWRGD,
+    input P1V1_PWRGD,
+
+
 // PORT0
-    inout BMC_GPIOA1,
-    inout BMC_GPIOA2,
-    inout BMC_GPIOA3,
-    inout BMC_GPIOC0,
+   inout R_FPGA_GPIO0,
+    inout R_FPGA_GPIO1,
+    inout R_FPGA_GPIO2,
+    inout R_FPGA_GPIO3,
+    inout R_FPGA_GPIO4,
+    inout R_FPGA_GPIO5,
+    inout R_FPGA_GPIO6,
+    inout R_FPGA_GPIO7,
 
-    inout BMC_GPIOC1,
-    inout BMC_GPIOC2,
-    inout BMC_GPIOC4,
-    inout BMC_GPIOC5,
 // PORT1
-    inout BMC_GPIOC6,
-    inout BMC_GPIOC7,
-    inout BMC_GPIOD3,
-    inout BMC_GPIOD6,
-
-    inout BMC_GPIOG0,
-    inout BMC_GPIOG1,
-    inout BMC_GPIOG3,
-    inout BMC_GPIOH0
+    inout R_FPGA_GPIO8,
+    inout R_FPGA_GPIO9,
+    inout R_FPGA_GPIO10,
+    inout R_FPGA_GPIO11,
+    inout R_FPGA_GPIO12,
+    inout R_FPGA_GPIO13,
+    inout R_FPGA_GPIO14,
+    inout R_FPGA_GPIO15
 );
     
 	wire 	clk, rst_l;
-    assign 	clk = CPLD_CLK_50M;
-    assign 	rst_l = CPLD_RST_N;
+    assign 	clk = FPGA_CLK_50M;
+    assign 	rst_l = VCORE_EN;
+    //assign 	rst_l = CPLD_RST_N;
+
+    assign 	USB_SWITCH_EN = 1'b1;
+    assign 	R_BMC_RSTN_FPGA = R_BMC_RSTN_EXT & R_CPU_POR_N;
 
 
     wire [7:0] PORT0;
-    assign {BMC_GPIOC5, BMC_GPIOC4, BMC_GPIOC2, BMC_GPIOC1,
-            BMC_GPIOC0, BMC_GPIOA3, BMC_GPIOA2, BMC_GPIOA1} = PORT0;
+    assign {R_FPGA_GPIO7,R_FPGA_GPIO6,R_FPGA_GPIO5,R_FPGA_GPIO4,
+            R_FPGA_GPIO3,R_FPGA_GPIO2,R_FPGA_GPIO1,R_FPGA_GPIO0} = PORT0;
     wire [7:0] PORT1;
-    assign {BMC_GPIOH0, BMC_GPIOG3, BMC_GPIOG1, BMC_GPIOG0, 
-            BMC_GPIOD6, BMC_GPIOD3, BMC_GPIOC7, BMC_GPIOC6} = PORT1;
+    assign {R_FPGA_GPIO15,R_FPGA_GPIO14,R_FPGA_GPIO13,R_FPGA_GPIO12,
+            R_FPGA_GPIO11,R_FPGA_GPIO10,R_FPGA_GPIO9,R_FPGA_GPIO8} = PORT1;
 
 
     wire [3:0] ledout;
